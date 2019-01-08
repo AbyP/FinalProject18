fee917baa7be70dfd755a13bf8f8b2880a2ab022
Aby <apachon@ucvts.org>
2019-01-08 17:58:31 +0000

    added dictionary to project

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+yearbook_member_list = ['Anna', 'Aby', 'Beatriz', 'Nicole', 'Andrew', 'Sara', 'Julia', 'Dexter', 'Kelley']
+
+class yearbook_member : #definition of class called human
+  """yearbook member class"""
+
+def __init__(self, name, age, gender, tech period, birthday):
+    """constructor for yearbook member class"""
+    self.name = name
+    self.age = age
+    self.hair = hair
+    self.eyes = eyes
+
+Anna = yearbook_member('Anna', '16', 'girl', ' ', ' ')
+Aby = yearbook_member('Aby', '17', 'girl', 'B 7/8 ', 'January 11')
+Beatriz = yearbook_member('Beatriz', '16', 'girl', 'B 3/4 ', 'March 14')
+Nicole = yearbook_member('Nicole', '16', 'girl', 'B 3/4 ', 'July 2')
+Sara = yearbook_member('Sara', '', 'girl', ' ', ' ')
+Andrew = yearbook_member('Andrew', '16', 'boy', ' ', ' ')
+Julia = yearbook_member('Beatriz', '16', 'girl', ' ', ' ')
+Dexter = yearbook_member('Beatriz', '16', 'guy', ' ', ' ')
+Kelley = yearbook_member('Kelley', '16', 'girl', 'B 7/8 ', ' ')
+
